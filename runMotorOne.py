5535cc1fdd1b2f83f866498b0358a60e0906409e
sauravDutt <sauravd069@gmail.com>
2025-04-04 21:34:01 +0100

    Just added some more stuff

--- runMotorOne.py
+++ runMotorOne.py
@@ -12,3 +12,28 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 GPIO.output(DIR, CW)
+
+MODE = (14, 15, 18)  # Microstep Resolution GPIO Pins
+GPIO.setup(MODE, GPIO.OUT)
+RESULATION = {
+    'Full' : (0, 0, 0),
+    'Half': (1, 0, 0),
+    '1/4' : (0, 1, 0),
+    '1/8' : (1, 1, 0),
+    '1/16' : (0, 0, 1),
+    '1/32' : (1, 0, 1)
+}
+GPIO.output(MODE, RESULATION['1/32'])
+
+step_count = SPR * 32
+delay = 0.0208 / 32
+
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
+
+sleep(0.5)
+# GPIO.output(DIR, CCW)
+
